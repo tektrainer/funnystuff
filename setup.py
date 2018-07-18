7e16443ea3e911312b452b381c8b62759040c01c
tektrainer <34774701+tektrainer@users.noreply.github.com>
2018-07-17 19:01:22 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funnystuff',
+      version='1.0',
+      description='Jokes jokes and more jokes',
+      url='http://github.com/tektrainer/funnystuff',
+      author='tektrainer',
+      author_email='lishat.tekmama@gmail.com',
+      license='MIT',
+      packages=['funnystuff'],
+      zip_safe=False)
